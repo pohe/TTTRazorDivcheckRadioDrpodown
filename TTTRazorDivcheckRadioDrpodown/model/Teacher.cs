43b3869ecb35cb40c631db5b7532100af9497bad
Poul Henriksen <pohe@easj.dk>
2021-03-17 08:55:23 +0100

    Add project files.

---
+++ TTTRazorDivcheckRadioDrpodown/model/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TTTRazorDivcheckRadioDrpodown.model
+{
+    public class Teacher
+    {
+        private static int nextId = 0;
+        public int  Id { get; set; }
+        public String Name { get; set; }
+        public bool IsMale { get; set; }
+        public List<String> Courses { get;  }
+
+        public string GetCoursesText
+        {
+            get { return "[ " + String.Join(",", Courses) + " ]"; }
+        }
+        public String WorkingLocation { get; set; }
+
+        public Teacher():this("Peter", true, "Roskilde")
+        {
+        }
+
+        public Teacher(string name, bool isMale, string workingLocation)
+        {
+            Name = name;
+            IsMale = isMale;
+            WorkingLocation = workingLocation;
+
+            Courses = new List<string>();
+            Id = ++nextId;
+        }
+    }
+}
